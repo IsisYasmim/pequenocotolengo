cb291786103a3fdc3c962c3badffaf9c0a7ffcb9
KaiqueVM <kaiqueviera08@gmail.com>
2025-05-30 14:46:48 -0300

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -1,1 +1,6 @@
-
+import streamlit as st
+from models import Funcionario
+from extensions import database as db
+from flask import Flask
+from datetime import datetime
+from sqlalchemy.orm import scoped_session
